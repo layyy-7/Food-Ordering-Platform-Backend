1b70ea75d5b329e9a7f917af1c2fc6efe482d526
Lay Shah <layshah07@gmail.com>
2024-07-27 18:22:38 +0530

    checkout flow and payment integration

--- src/models/restaurant.ts
+++ src/models/restaurant.ts
@@ -1,7 +1,13 @@
-import mongoose from "mongoose";
+import mongoose, { InferSchemaType } from "mongoose";
 
 const menuItemSchema=new mongoose.Schema
 ({
+    _id:
+    {
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        default:()=>new mongoose.Types.ObjectId()
+    },
     name:
     {
         type:String,
@@ -13,6 +19,8 @@ const menuItemSchema=new mongoose.Schema
         required:true
     }
 });
+
+export type MenuItemType=InferSchemaType<typeof menuItemSchema>;
 
 const restaurantSchema=new mongoose.Schema
 ({
